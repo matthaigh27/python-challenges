750b8caaa342ba94f8aa8d845924617f36492157
Matthaigh27 <matthaigh27@gmail.com>
2019-01-15 19:07:17 +1100

    factors challenge

---
+++ factors.py
@@ -0,0 +1,19 @@
+def fact(x):
+    cond = True
+
+    if x != 1:
+        for div in range (2, x // 2 + 1):
+            if x % div == 0:
+                factors.append(div)
+                factors.append(x//div)
+                return fact(x // div)
+            elif div == x // 2 and x % div != 0:
+                return fact(1)
+    else:
+        fac = str(factors)
+        factors.clear()
+        return fac
+
+factors = []
+print(fact(22))
+print(fact(14))
