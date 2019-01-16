77aa41dc3d7a05d229dfe467a6ceac1212ddaa7a
Matthaigh27 <matthaigh27@gmail.com>
2019-01-16 16:43:02 +1100

    some more progress on factorising

--- factors.py
+++ factors.py
@@ -1,17 +1,20 @@
 def fact(x):
-    factors = []
-    cond = True
-    while(cond):
-        if x // 2 + 1 == 2:
-            factors.append(2)
-            return factors
-        else:
-            for div in range(2,x // 2 + 1):
-                if x % div == 0:
-                    factors.append(div)
-                    x = x // div
-                    break
-                elif div == x // 2:
-                    factors.append(x)
-                    return factors
-print(fact(64))
+    factors = [1]
+    start = 2
+    c = 0
+    while x != 1:
+        for div in range(start,x // 2 + 1):
+            print(x,div)
+            c += 1
+            if x % div == 0:
+                factors.append(div)
+                start = div
+                x = x // div
+                break
+            elif div == x // 2:
+                factors.append(x)
+                print(c)
+                return factors
+    print(c)
+    return factors
+print(fact(7))
